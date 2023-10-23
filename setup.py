a60e7b086642aca39b5d194c96b8000840e4d7d7
code-Cells <96440431+code-Cells@users.noreply.github.com>
2023-10-23 06:21:08 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-  name="flitton-fib-rs",
+  name="fib-rs",
   version="0.1",
   rust_extensions=[RustExtension(
-    ".flitton_fib_rs.flitton_fib_rs",
+    ".fib_rs.fib_rs",
     path="Cargo.toml", binding=Binding.PyO3)
   ],
-  packages=["flitton_fib_rs"],
+  packages=["fib_rs"],
   classifiers=[
     "License :: OSI Approved :: MIT License",
     "Development Status :: 3 - Alpha",
