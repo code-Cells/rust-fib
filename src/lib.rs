bb81f1d676846693e336aac8a98cc0ca9135992c
code-Cells <96440431+code-Cells@users.noreply.github.com>
2023-10-23 06:18:46 -0300

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+[#pyfunction]
+fn say_hello() {
+    println!("Saying hello from Rust!");
+}
+
+#[pymodule]
+fn fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(say_hello));
+    Ok(())
+}
