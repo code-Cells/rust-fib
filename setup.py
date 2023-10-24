eb9e03f2b3fe505984658677745654d849fc56bd
code-Cells <96440431+code-Cells@users.noreply.github.com>
2023-10-24 05:57:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python
-
-from importlib.metadata import entry_points
 from setuptools import dist
 dist.Distribution().fetch_build_eggs(['setuptools_rust'])
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
+
 setup(
-  name="fib-rs",
-  version="0.2",
-  rust_extensions=[RustExtension(
-    ".fib_rs.fib_rs",
-    path="Cargo.toml", binding=Binding.PyO3)
-  ],
-  packages=["fib_rs"],
-  classifiers=[
-    "License :: OSI Approved :: MIT License",
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    "Programming Language :: Python",
-    "Programming Language :: Rust",
-    "Operating System :: POSIX",
-    "Operating System :: MacOS :: MacOS X",
-  ],
-  zip_safe=False,
-  entry_points={
-    'console_scripts': [
-        'fib-number = flitton_fib_rs.fib_number_command:fib_number_command',
-    ],
-  },
+    name="flitton-fib-rs",
+    version="0.1",
+    rust_extensions=[RustExtension(
+        ".fib_rs.fib_rs",
+        path="Cargo.toml", binding=Binding.PyO3)],
+    packages=["fib_rs"],
+    classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "Programming Language :: Rust",
+            "Operating System :: POSIX",
+            "Operating System :: MacOS :: MacOS X",
+        ],
+    entry_points={
+        'console_scripts': [
+            'fib-number = fib_rs.fib_number_command:fib_number_command',
+        ],
+    },
+    zip_safe=False,
 )
